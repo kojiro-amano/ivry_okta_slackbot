79bf1b36a9db914244bf5cbdd805ee6963cccabd
test <k.amano51@gmail.com>
2023-10-21 16:07:34 +0900

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.2.6"
+  required_version = ">=1.2.6"
   backend "s3" {
     bucket         = "stg-okta-slackbot-terraform-state"
     key            = "terraform.tfstate"
